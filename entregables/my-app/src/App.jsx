64d0d831fcb79c08ca8d30838ae5bdf545cb3617
Jinshuo <jinshuo.xia8@gmail.com>
2025-05-15 17:45:57 +0200

    fps meter done

---
+++ entregables/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import FPSMeter from './components/FPSMeter';
+import HeavyList from './components/HeavyList';
+
+function App() {
+    return (
+        <>
+            <h1>Demo FPS</h1>
+            <FPSMeter />
+            <HeavyList />
+        </>
+    );
+}
+
+export default App;
